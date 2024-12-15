40f8544abebfac6a14cd6eb9310554df72ca6bae
john <john.liu@mayfull.com.tw>
2024-12-15 16:32:38 +0800

    購買商品數量金額計算

--- shop/js/shopcartorder.js
+++ shop/js/shopcartorder.js
@@ -3,8 +3,20 @@ function order_data_set() {
 	  showOrderListLayout: true,
 	  orderList: orderList,
 	  orderInfo: orderInfo,
-	  goReturnOrder(orderIndex) {
-		this.orderInfo.oritjine=this.orderInfo.oritjine+this.orderInfo.oritjine;
+	  minusItem(orderIndex) {
+		if(this.orderList[orderIndex].orderdata.acc>1)
+		{
+			var tempnum =Number(this.orderList[orderIndex].orderdata.acc);
+			this.orderList[orderIndex].orderdata.acc=String(tempnum-1);
+			var tempjline =Number(this.orderList[orderIndex].orderdata.jine.replace(/,/g, ""));
+			this.orderInfo.oritjine=this.orderInfo.oritjine-tempjline;
+		}
+	  },
+	  addItem(orderIndex) {
+		var tempnum =Number(this.orderList[orderIndex].orderdata.acc);
+		this.orderList[orderIndex].orderdata.acc=String(tempnum+1);
+		var tempjline =Number(this.orderList[orderIndex].orderdata.jine.replace(/,/g, ""));
+		this.orderInfo.oritjine=this.orderInfo.oritjine+tempjline;
 	  }}
   }
 
